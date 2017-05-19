010e2ecb2d2e892eb3fd51fb9bcbbd2e2551a1c3
wg <za-wanggang@zaonline.com>
2017-05-19 18:55:26 +0800

    update

--- src/main/java/cn/wglgg/chat/webchat/web/ChatController.java
+++ src/main/java/cn/wglgg/chat/webchat/web/ChatController.java
@@ -1,10 +1,3 @@
-/*
-* Copyright 2017 Zhongan.io All right reserved. This software is the
-* confidential and proprietary information of Zhongan.io ("Confidential
-* Information"). You shall not disclose such Confidential Information and shall
-* use it only in accordance with the terms of the license agreement you entered
-* into with Zhongan.io.
-*/
 package cn.wglgg.chat.webchat.web;
 
 import cn.wglgg.chat.webchat.service.MsgProducer;
